c1c4a0bb9d4207da3d55cc49069cced3c8925787
Bulat <okhotnikov_99@bk.ru>
2025-09-10 00:32:42 +0300

    PassportDTO Added

--- src/main/java/com/UserPassportBoot/Controllers/UserController.java
+++ src/main/java/com/UserPassportBoot/Controllers/UserController.java
@@ -28,22 +28,22 @@ import java.sql.SQLOutput;
 @RequestMapping("/users")
 public class UserController {
 
-private final UserService userService;
-private final UserValidator userValidator;
-private final PassportService passportService;
-private final ModelMapper modelMapper;
-@Autowired
+    private final UserService userService;
+    private final UserValidator userValidator;
+    private final PassportService passportService;
+    private final ModelMapper modelMapper;
+    @Autowired
     public UserController(UserService userService, UserValidator userValidator, PassportService passportService, ModelMapper modelMapper) {
-    this.userService = userService;
-    this.userValidator = userValidator;
-    this.passportService = passportService;
-    this.modelMapper = modelMapper;
+        this.userService = userService;
+        this.userValidator = userValidator;
+        this.passportService = passportService;
+        this.modelMapper = modelMapper;
 
-}
-/*
-отображение всех пользователей, кастомная пагинация только в этом методе, сортировка через RequestParam по
-полям сущности User, также есть кастомная сортировка по возрасту
- */
+    }
+    /*
+    отображение всех пользователей, кастомная пагинация только в этом методе, сортировка через RequestParam по
+    полям сущности User, также есть кастомная сортировка по возрасту
+     */
     @GetMapping
     public String index(Model model,
                         @RequestParam(defaultValue = "0") int page,
@@ -67,12 +67,12 @@ private final ModelMapper modelMapper;
     public String show(Model model, @PathVariable("id") int id, @ModelAttribute("passport") Passport passport){
         model.addAttribute("user", userService.findUserById(id));
         model.addAttribute("passport", passportService.findByOwner(userService.findUserById(id)));
-    return "user/show";
+        return "user/show";
     }
     // перенаправление на страницу создания user
     @GetMapping("/new")
     public String newUser(@ModelAttribute("user") UserDTO userDTO){
-return "user/new";
+        return "user/new";
     }
 /*
  проверка валидности данных, и последующее сохранение при правильно, введённых данных, иначе
@@ -83,41 +83,41 @@ return "user/new";
     public String create(@ModelAttribute("user") @Valid UserDTO userDTO, BindingResult bindingResult){
 
         User user = convertToUser(userDTO);
-userValidator.validate(user, bindingResult);
+        userValidator.validate(user, bindingResult);
         if(bindingResult.hasErrors()){
             return "user/new";
         }
         userService.save(user);
         return "redirect:/users";
     }
-/*
-перенаправление на страницу изменения данных пользователя, передаётся в модель также сам user по id
- */
+    /*
+    перенаправление на страницу изменения данных пользователя, передаётся в модель также сам user по id
+     */
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id){
         model.addAttribute("user", userService.findUserById(id));
         return "user/edit";
     }
-// проверка на валидность данных, при правильных изменениях redirect на страницу с пользвоателями
-// , иначе остаётся на этой же станице
+// проверка на валидность данных, при правильных изменениях redirect на страницу с пользователями
+//, иначе остаётся на этой же станице
 
     @PostMapping("/{id}")
     public String update(@ModelAttribute("user") @Valid User user,
                          @PathVariable("id") int id,
                          BindingResult bindingResult){
         userValidator.validate(user, bindingResult);
-    if(bindingResult.hasErrors())
-        return "user/edit";
+        if(bindingResult.hasErrors())
+            return "user/edit";
 
-    user.setId(id);
-userService.update(user.getId(), user);
+        user.setId(id);
+        userService.update(user.getId(), user);
         return "redirect:/users";
 
     }
     // перенаправление на страницу удаления пользователя
     @GetMapping("/{id}/delete")
     public String delete(Model model, @PathVariable("id") int id){
-model.addAttribute("user", userService.findUserById(id));
+        model.addAttribute("user", userService.findUserById(id));
         return "user/delete";
     }
     // удаление пользователя
@@ -134,7 +134,7 @@ model.addAttribute("user", userService.findUserById(id));
 
 
     private User convertToUser(UserDTO userDTO){
-return modelMapper.map(userDTO, User.class);
+        return modelMapper.map(userDTO, User.class);
 
     }
 
--- src/test.sql
+++ src/test.sql
@@ -1,1 +1,31 @@
-update java_users set password='test' where id = 7
+CREATE TABLE java_users
+(
+    id            INT AUTO_INCREMENT NOT NULL,
+    username      VARCHAR(255)       NULL,
+    age           INT                NULL,
+    email         VARCHAR(255)       NULL,
+    created_at    datetime           NULL,
+    date_of_birth date               NULL,
+    gender        VARCHAR(100)       NULL,
+    password      VARCHAR(255)       NULL,
+    `role`        VARCHAR(100)       NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+);
+
+CREATE TABLE passport
+(
+    id               INT AUTO_INCREMENT NOT NULL,
+    serial           VARCHAR(4)         NOT NULL,
+    number           VARCHAR(6)         NOT NULL,
+    user_id          INT                NOT NULL,
+    controlDigit     INT                NOT NULL,
+    dateOfReceipt    date               NOT NULL,
+    expirationDate   date               NOT NULL,
+    statusOfPassport VARCHAR(255)       NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+);
+
+ALTER TABLE passport
+    ADD CONSTRAINT passport_java_users_id_fk FOREIGN KEY (user_id) REFERENCES java_users (id) ON DELETE NO ACTION;
+
+CREATE INDEX passport_java_users_id_fk ON passport (user_id);
